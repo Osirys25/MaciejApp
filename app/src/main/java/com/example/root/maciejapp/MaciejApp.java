f1b1c5ee74c4abb441df36bd2cf7e10dfb1265fc
Mikolaj Formanski <mikolaj.formanski@identt.pl>
2017-01-20 16:40:03 +0100

    Initialize commit

---
+++ app/src/main/java/com/example/root/maciejapp/MaciejApp.java
@@ -0,0 +1,13 @@
+package com.example.root.maciejapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaciejApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maciej_app);
+    }
+}
