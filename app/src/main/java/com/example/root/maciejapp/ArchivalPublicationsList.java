ef3579e2c31a49523ae6ff561d1ebdb4e288a2e1
Maciej Gruszczyński <228131@student.pwr.edu.pl>
2017-02-23 21:01:52 +0100

    Zapisywanie ogłoszeń lokalnie

---
+++ app/src/main/java/com/example/root/maciejapp/ArchivalPublicationsList.java
@@ -0,0 +1,84 @@
+package com.example.root.maciejapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by maciej on 19.02.17.
+ */
+public class ArchivalPublicationsList extends AppCompatActivity{
+    private ListView listView;
+    private ArrayList<String> listItems=new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private Button refreshButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archival_publications);
+        refreshButton = (Button) findViewById(R.id.refreshButton);
+        hideRefreshButton();
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new ArchivalPublicationsList.OnPublicationClickListener());
+        PublicationsLoader.activitiesToUpDate.add(this); //handler to this activity, allows PublicationsLoader to upDate this Activity data
+        upDateArchivalPublicationsList();
+    }
+
+    private class OnPublicationClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            Intent intent = new Intent(ArchivalPublicationsList.this, SinglePublication.class);
+            intent.putExtra("current", false); //powiadamiam aktywność SinglePublication, że ma wyświetlić archiwalne ogłoszenia
+            intent.putExtra("position", position);
+            startActivity(intent);
+
+        }//onItemClick(...)
+    }
+
+    public void upDateArchivalPublicationsList(){
+
+        switch(PublicationsLoader.publicationsStatus){
+            case RS_DS_US: //go down
+            case RF_DS_US: //go down
+            case RS_DS_UF: //go down
+            case RF_DS_UF: //go down
+            case RS_DF: //go down
+            case RS:{
+                hideRefreshButton();
+                listItems.clear();
+                for (int ii = 1; ii < PublicationsLoader.getpubsNumber(); ii++) {
+                    listItems.add(PublicationsLoader.getPubsDataArray()[ii][0]);
+                }
+                adapter.notifyDataSetChanged();
+                break;
+            }
+            case RF_DF:{
+                showRefreshButton();
+                break;
+            }
+        }//switch
+
+    }
+
+    public void showRefreshButton(){
+        refreshButton.setVisibility(View.VISIBLE);
+    }
+
+    public void hideRefreshButton(){
+        refreshButton.setVisibility(View.GONE);
+    }
+
+    public void refresh(View view){
+            new PublicationsLoader().execute();
+    }
+
+}
--- app/src/main/java/com/example/root/maciejapp/SinglePublication.java
+++ app/src/main/java/com/example/root/maciejapp/SinglePublication.java
@@ -1,47 +1,102 @@
 package com.example.root.maciejapp;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * Created by maciej on 17.02.17.
  */
 public class SinglePublication extends AppCompatActivity {
 
-    WebView webView;
+    private WebView webView;
+    private String[] publicationDataArray;
+    private Button refreshButton;
+    private LinearLayout buttonLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_publication);
-        loadWebViewAndTitle();
+        PublicationsLoader.activitiesToUpDate.add(this); //handler to this activity, allows PublicationsLoader to upDate this Activity data
+        refreshButton = (Button) findViewById(R.id.refreshButton);
+        buttonLayout = (LinearLayout) findViewById(R.id.buttonLayout);
+        upDatePublication();
     }
 
 
+    private void loadPublicationData(){
 
-    private void loadWebViewAndTitle(){
+        publicationDataArray = new String[2];
 
-        String[] publicationDataArray = new String[2];
-        int position = getIntent().getIntExtra("position",0);
+        switch(PublicationsLoader.publicationsStatus){
+            case RS_DS_US: //go down
+            case RF_DS_US: //go down
+            case RF_DS_UF: //go down
+            case RS_DS_UF: //go down
+            case RS_DF: //go down
+            case RS:{
+                int position = getIntent().getIntExtra("position", 0);
+                boolean isCurrent = getIntent().getBooleanExtra("current", true);
 
-        publicationDataArray[0]=JSONOperator.getPubsDataArray()[position][0];
-        publicationDataArray[1]=JSONOperator.getPubsDataArray()[position][1];
+                if (isCurrent) {
+                    showRefreshButton();
+                    publicationDataArray[0] = PublicationsLoader.getPubsDataArray()[0][0];
+                    publicationDataArray[1] = PublicationsLoader.getPubsDataArray()[0][1];
+                }
+                else{
+                    hideRefreshButton();
+                    position += 1; //poprawka na pozycję archiwalnych ogłoszeń
+                    publicationDataArray[0] = PublicationsLoader.getPubsDataArray()[position][0];
+                    publicationDataArray[1] = PublicationsLoader.getPubsDataArray()[position][1];
+                }
+                break;
+            }
+            case RF_DF:{
+                showRefreshButton();
+                publicationDataArray[0] = "Aktualne ogłoszenia";
+                publicationDataArray[1] = "";
+                break;
+            }
+        }//switch
 
+        //przyda się Log.d("MyLog",publicationDataArray[0]+'\n'+publicationDataArray[1]);
+    }
 
-        Log.d("MyLog",publicationDataArray[0]+'\n'+publicationDataArray[1]);
+    private void loadWebView(){
 
         webView = (WebView)findViewById(R.id.webView);
         webView.loadData(publicationDataArray[1], "text/html; charset=utf-8", "UTF-8");
 
-        setTitle(publicationDataArray[0]);
     }//loadWebView()
 
+    private void loadTitle(){
+        setTitle(publicationDataArray[0]);
+    }
 
+    public void upDatePublication(){
+        loadPublicationData();
+        loadTitle();
+        loadWebView();
+    }
 
+    public void showRefreshButton(){
+        refreshButton.setVisibility(View.VISIBLE);
+        buttonLayout.setVisibility(View.VISIBLE);
+    }
 
+    public void hideRefreshButton(){
+        refreshButton.setVisibility(View.GONE);
+        buttonLayout.setVisibility(View.GONE);
+    }
+
+    public void refresh(View view){
+            new PublicationsLoader().execute();
+    }
 
 
 
